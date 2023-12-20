fb91e2034c7c9ec42d4154a09ad67d92996cfda9
KeshiNguyen <ducnd06112001@gmail.com>
2023-12-20 21:08:33 +0700

    FoodCategory

--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -72,6 +72,55 @@ class FoodFactory extends Factory
             'https://www.youtube.com/watch?v=WI_ut7dTRQ8&pp=ugMICgJ2aRABGAHKBR5jw7RuZyB0aOG7qWMgICAnQ0hJQ0tFTiBQQVJNJyw%3D',
         ];
 
+        $foodImage = [
+            'https://images.unsplash.com/photo-1610452220299-5edf90b8a6ed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8J1BoJUUxJUJCJTlGJTIwRyVDMyVBMCclMkN8ZW58MHx8MHx8fDA%3D',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-G3Ejs4UdeSWwSYHOpPRDhVrKv1Q4nRSl8A&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtEZEZW8gTKQ31wQpHAJ9-gqE1OOuFacReuzWJQ8_Ubc8Z05dFVIE6rVpqP22I3D_1lGA&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY0Jd1GIVbhKfXD1uhaWouhuNtJCK9rttavw&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1iM_3s3RG5EURgfMmZlQDGg8GU7648MkvjA&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiFRqSjKzraxO3ds4Rj0FQGFLDXVzijAmfQQ&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd6kZMyxhBoq-JXe5Dq6sAvIt-24Fz9G3Jtw&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScifgQ0kbKSJIUB_QRmPzN033EhdI3Wqh8Kw&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCAm3l24vlpQvMHk1JHkXr6ZzxFPdylkOpGg&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMX_TTj9Ocf1wfteAeG0nSTVAsnutcd-p2hw&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMUxXREijRWWvolnsRhfSbX6d4nIOZ4EQbhw&usqp=CAU',
+            'https://images.unsplash.com/photo-1610192244261-3f33de3f55e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8J01BU0FMQSUyMERPU0EnJTJDfGVufDB8fDB8fHww',
+            'https://images.unsplash.com/photo-1512058533999-106ee01bf777?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8J0MlQzYlQTBNJTIwVEglRTElQkElQUNQJTIwQyVFMSVCQSVBOE0lMjBQQUVMTEEnJTJDfGVufDB8fDB8fHww',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6XQh23Wr1NeHJRgerQPpjSJn6DA8RrsFgsw&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg2dG9-oJ4FyaC4I7Hna4VtPbigEYvQCjZyQ&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCvsxiIOfOG-_ZT9eYdp2XEtqxSudJoQOJmQ&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqjQNTueryDli37xkOHqSgkLlm30jPySkgYg&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlQhNG0PhX3kgYS-nS9pssH9n0m_DOF9_ebw&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkLynh9NS2nbtiM_jA2fhBn2J52_p1PuwOXQ&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKvNQ5LOSbl-7gxmfXIjfzSErZW4tR4i_OIQ&usqp=CAU',
+        ];
+
+
+
+
+        $foodDescription = [
+            'Sự hòa quyện của nước dùng phô mai từ xương gà, thịt gà mềm mịn, bún giòn, và rất nhiều gia vị hảo hạng, Phở Gà là bữa sáng hoặc trưa trọn vị tinh tế của ẩm thực Việt Nam',
+            ' Hương vị tinh tế của nước dùng từ cua, sự ngon miệng của bún mềm mịn, và viên riêu béo ngon, Bún Riêu là một tác phẩm nghệ thuật ẩm thực đậm đà và đặc sắc.',
+            'Mỗi cuộn nhẹ nhàng của Gỏi Cuốn là một bức tranh màu sắc, với những thành phần tươi ngon như tôm, thịt, bún, và rau sống, tạo nên một trải nghiệm ngon miệng không thể quên.',
+            'Cơm Niêu là sự kết hợp hoàn hảo giữa hạt cơm giòn và hương vị thơm ngon của mực, tôm, và thịt. Một bữa ăn truyền thống ngon lành.',
+            'Hòa quyện giữa vị bánh bao mềm mịn, nhân thịt thơm ngon, và hương vị đặc trưng của xôi nước, Bánh Bao Nhân Thịt Xôi Nước là một lựa chọn ngon miệng cho bữa ăn nhẹ.',
+            'Nước dùng cay nồng, thịt bò mềm và bún giòn tạo nên một bát Bún Bò Huế ngon lành, đầy đủ hương vị và mùi thơm.',
+            'Bánh mì giòn tan, pate mềm mịn, xúc xích thơm ngon và rau sống tươi ngon, tạo nên một bữa sáng nhanh chóng và ngon miệng.',
+            'Thịt bò lúc lắc được xào nhanh trong nồi nóng, với hương vị thơm ngon và hấp dẫn. Bò Lúc Lắc thường ăn kèm với cơm trắng và rau sống.',
+            'Cơm trắng mềm, thịt gà mềm và nước lẩu hải sản đặc trưng, Cơm Gà Hải Nam là sự phối hợp hài hòa giữa hương vị và hình thức.',
+            'Bánh Canh Cua hấp dẫn với nước dùng thơm nồng từ cua, nhân thịt thơm ngon và bánh canh mềm mịn, là một hành trình ẩm thực đầy trải nghiệm.',
+            'Một hương vị đặc trưng của bắp ngô rang giòn, thơm ngon vị bơ, tạo nên một loại ăn nhẹ phổ biến và ngon miệng.',
+            'Một món ăn Ấn Độ với bánh dosa mỏng giòn và nhân masala độc đáo, MASALA DOSA là một cung đường hương vị tuyệt vời.',
+            'Một món ăn đặc trưng của Tây Ban Nha, CƠM THẬP CẨM PAELLA là sự kết hợp độc đáo của cơm và các loại hải sản, thịt, và rau củ.',
+            'Gỏi Đu Đủ tươi ngon với hương vị chua ngọt, giòn giòn, kết hợp cùng nước mắm pha chua ngọt, là một lựa chọn hoàn hảo cho bữa ăn nhẹ.',
+            'Cơm trắng thơm, thịt gà mềm, và gia vị độc đáo, CƠM GÀ SINGAPORE là một món ăn độc đáo từ vùng đất sáng tạo này.',
+            'Một món ăn ngon từ Canada, POUTINE là sự kết hợp hấp dẫn giữa khoai tây chiên, sốt phô mai và thịt hầm ngon.',
+            'Một món ăn truyền thống của Mexico, TACO với bánh taco giòn tan, nhân thịt thơm ngon và các loại rau sống tươi ngon.',
+            'Bánh Hạnh Nhân giòn ngon, hương vị độc đáo của hạnh nhân, là một lựa chọn tuyệt vời cho thời gian trà chiều.',
+            'Bánh Mì Pháp với vỏ giòn, nhân thịt thơm ngon, và hương vị đặc trưng của phô mai và rau sống.',
+            'Thịt gà chiên giòn, phủ lớp sốt cà chua và phô mai, CHICKEN PARM là một món ăn ngon không thể cưỡng.',
+        ];
+
         $index = $this->faker->numberBetween(0, 19);
 
         return [
@@ -80,12 +129,14 @@ class FoodFactory extends Factory
             'deleted_flag' => $this->faker->numberBetween(0, 1),
             // 'name' => $this->faker->sentence,
             'name' => $foodName[$index],
-            'description' => $this->faker->paragraph,
+            // 'description' => $this->faker->paragraph,
+            'description' =>$foodDescription[$index],
             'views' => $this->faker->numberBetween(100, 1000),
             'total_like' => $this->faker->numberBetween(10, 100),
             'food_category_regions_id' => $this->faker->randomElement($regionIds),
             'food_categories_id' => $this->faker->randomElement($categoryIds),
-            'thumbnail' => $this->faker->imageUrl(),
+            'thumbnail' => $foodImage[$index],
+            // 'thumbnail' => $this->faker->imageUrl(),
             // 'video' => $this->faker->url,
             'video' => $foodVideo[$index],
             'prep_time' => $this->faker->numberBetween(10, 60),
